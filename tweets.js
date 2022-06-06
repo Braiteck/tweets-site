6fc2b9e8721322c4ec1ee3a326657b633947fdaf
Vitaliy Sulimenko <prevetal@gmail.com>
2022-06-06 12:52:11 +0300

    Update tweets.js

--- tweets.js
+++ tweets.js
@@ -43,7 +43,7 @@ class Tweets {
                 },
                 Bootstrap: [
                     // '/dns4/ws-star.discovery.cybernode.ai/tcp/4430/p2p/QmUgmRxoLtGERot7Y6G7UyF6fwvnusQZfGR15PuE6pY3aB',
-                    '/dns4/ws-star.discovery.cybernode.ai/tcp/4430/wss/p2p/QmUgmRxoLtGERot7Y6G7UyF6fwvnusQZfGR15PuE6pY3aB',
+                    `/dns4/ws-star.discovery.cybernode.ai/tcp/4430/wss/p2p/${this.particleFrom}`,
                     // '/dns6/ipfs.thedisco.zone/tcp/4430/wss/p2p/12D3KooWChhhfGdB9GJy1GbhghAAKCUR99oCymMEVS4eUcEy67nt',
                     // '/dns4/ipfs.thedisco.zone/tcp/4430/wss/p2p/12D3KooWChhhfGdB9GJy1GbhghAAKCUR99oCymMEVS4eUcEy67nt',
                 ],
