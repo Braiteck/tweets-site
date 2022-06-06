a0fb7c0ed2559d0ef346cbe6faddc496709d3e9d
Vitaliy Sulimenko <prevetal@gmail.com>
2022-06-06 12:41:48 +0300

    +

--- tweets.js
+++ tweets.js
@@ -20,16 +20,50 @@ class Tweets {
             .then(data => this.loadData = data.txs.sort((a, b) => Date.parse(b.timestamp) - Date.parse(a.timestamp)))
 
         this.node = await Ipfs.create({
-            repo: String(Math.random() + Date.now()),
-            init: { alogorithm: 'ed25519' }
+            repo: 'ipfs-repo-cyber',
+            init: true,
+            start: true,
+            relay: {
+                enabled: true,
+                hop: {
+                    enabled: true,
+                },
+            },
+            EXPERIMENTAL: {
+                pubsub: true,
+            },
+            config: {
+                Addresses: {
+                    Swarm: [
+                        // '/dns4/star.thedisco.zone/tcp/9090/wss/p2p-webrtc-star',
+                        // '/dns6/star.thedisco.zone/tcp/9090/wss/p2p-webrtc-star',
+                        '/dns4/ws-star.discovery.cybernode.ai/tcp/443/wss/p2p-webrtc-star',
+                        // '/dns4/ws-star.discovery.cybernode.ai/tcp/4430/wss/p2p/QmUgmRxoLtGERot7Y6G7UyF6fwvnusQZfGR15PuE6pY3aB',
+                    ],
+                },
+                Bootstrap: [
+                    // '/dns4/ws-star.discovery.cybernode.ai/tcp/4430/p2p/QmUgmRxoLtGERot7Y6G7UyF6fwvnusQZfGR15PuE6pY3aB'
+                    '/dns4/ws-star.discovery.cybernode.ai/tcp/4430/wss/p2p/QmUgmRxoLtGERot7Y6G7UyF6fwvnusQZfGR15PuE6pY3aB',
+                    // '/dns6/ipfs.thedisco.zone/tcp/4430/wss/p2p/12D3KooWChhhfGdB9GJy1GbhghAAKCUR99oCymMEVS4eUcEy67nt',
+                    // '/dns4/ipfs.thedisco.zone/tcp/4430/wss/p2p/12D3KooWChhhfGdB9GJy1GbhghAAKCUR99oCymMEVS4eUcEy67nt',
+                ],
+            }
         })
     }
 
     async parseTweets() {
         await this.loadTweets()
 
+        const responseDag = await this.node.dag.get(this.neuron, {
+            localResolve: false,
+        })
+
+        console.log(responseDag.value)
+
         for (const el of this.loadData) {
-            for await (const data of this.node.cat(el.tx.value.msg[0].value.links[0].to)) {
+            let cid = el.tx.value.msg[0].value.links[0].to
+
+            for await (const data of this.node.cat(cid)) {
                 this.tweets.push(new TextDecoder().decode(data))
             }
 
